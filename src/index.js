428a3825316a7f83110ee86ee6c43374414504b2
mariartve <110705321+mariartve@users.noreply.github.com>
2024-04-19 18:43:16 -0600

    Revert "trying to deploy"
    This reverts commit 7c5e4e37783d7fcf116d466c15829ffdd159fa7c.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import './src/styles/main.scss';
-import { initInputValidation, initSearchButton } from './src/js/form-validation';
+import './styles/main.scss';
+import { initInputValidation, initSearchButton } from './js/form-validation';
 
 (function init() {
   initInputValidation();
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -73,7 +73,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './src/index.html',
     }),
   ],
 };
